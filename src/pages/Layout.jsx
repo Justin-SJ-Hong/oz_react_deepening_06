d30d05afbc7ba967724593be0ad8552da5dfbb90
Justin-SJ-Hong <madwolves98@gmail.com>
2025-07-03 16:53:26 +0900

    어드민 페이지 설정

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
 // 1. Outlet을 사용하여 모든 라우팅에서 보이게 해야합니다.
 // 2. 각 Link를 연동하여 알맞는 주소로 전송 해야 합니다.
+
+const randomNumber = () => {
+  return Math.random();
+};
+
+let randomPath = `/admin/${randomNumber()}`;
 
 function Layout() {
   return (
@@ -13,12 +19,14 @@ function Layout() {
       </header>
       <div className="admin-content">
         <div className="sidebar">
-          <Link to="#">대시보드</Link>
-          <Link to="#">사용자</Link>
-          <Link to="#">설정</Link>
-          <Link to="#">잘못된 페이지</Link>
+          <Link to="/admin">대시보드</Link>
+          <Link to="/admin/users">사용자</Link>
+          <Link to="/admin/settings">설정</Link>
+          <Link to={randomPath}>잘못된 페이지</Link>
         </div>
-        <div className="main-content"></div>
+        <div className="main-content">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
